63c0f7b908626d4aeab18d8c8c1afe7381498318
vishal kumar <vishakumar041200@gmail.com>
2025-03-12 14:54:22 +0530

    lecture

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import { Menu, School } from "lucide-react";
 import React, { useEffect } from "react";
-import DarkMode from "../Darkmode";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -12,6 +11,7 @@ import {
 } from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import DarkMode from "@/DarkMode";
 import {
   Sheet,
   SheetClose,
